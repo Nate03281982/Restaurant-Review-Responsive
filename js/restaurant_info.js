8cb1c32fbfc50cc3f36520ea1ee4fcdbeffbdddb
Nataniel Ben-Ami <Nate0281982@gmail.com>
2018-07-20 23:37:14 +0300

    htmls

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -24,7 +24,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'AIzaSyARWPjWvRXJJAURJuuqH5S62Rwrm_f6Gk8',
+        mapboxToken: 'pk.eyJ1IjoibmF0ZWJlbmFtaSIsImEiOiJjampteXYxenUxcmpuM2ttMHB5NzRqdW9zIn0.SyefywoYmtgFLbfMWq88eg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
